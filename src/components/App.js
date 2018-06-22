24bf10194bd9be794a9ce0d21eef5c9513afc6ab
Vladimir Musikhin <ask@vi4.me>
2018-06-22 14:49:13 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-4">
+                    Form
+                </div>
+                <div className="col">
+                    Map
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
